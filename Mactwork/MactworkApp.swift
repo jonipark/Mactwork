38e0d1721dd40ddfd53ce57c033b1c8f8207895a
Joeun Park <park49j@mtholyoke.edu>
2023-03-28 21:48:25 -0400

    Initial Commit

---
+++ Mactwork/MactworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MactworkApp.swift
+//  Mactwork
+//
+//  Created by Joeun Park on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MactworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
